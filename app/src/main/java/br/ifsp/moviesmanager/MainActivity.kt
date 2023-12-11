2bb0be33888d80ff347c73408b25c9a180c580c4
erlonprifsp <erlon.rodrigues@aluno.ifsp.edu.br>
2023-12-11 09:12:05 -0300

    inicio do projeto

---
+++ app/src/main/java/br/ifsp/moviesmanager/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.ifsp.moviesmanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
