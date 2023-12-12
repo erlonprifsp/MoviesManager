f7bd95986f1a74e4561a4dd8117e474b5ac47fbe
erlonprifsp <erlon.rodrigues@aluno.ifsp.edu.br>
2023-12-12 13:41:32 -0300

    correção da classe MovieDao

--- app/src/main/java/br/edu/ifsp/scl/sdm/moviesmanager/model/dao/MovieDao.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/moviesmanager/model/dao/MovieDao.kt
@@ -1,5 +1,6 @@
 package br.edu.ifsp.scl.sdm.moviesmanager.model.dao
 
+//  importações de classes e anotações do Room
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
@@ -10,14 +11,19 @@ import br.edu.ifsp.scl.sdm.moviesmanager.model.entity.Movie
 @Dao
 interface MovieDao {
     companion object {
-        const val MOVIE_TABLE = "movie"
+        const val MOVIE_TABLE = "movie" // define uma constante MOVIE_TABLE com o valor movie
     }
+
+    // implementação do CRUD
+
     @Insert
-    fun createMovie(movie: Movie)
+    fun createMovie(movie: Movie) // insere um objeto Movie no banco de dados
     @Query("SELECT * FROM $MOVIE_TABLE")
-    fun retrieveMovies(): List<Movie>
+    fun retrieveMovies(): List<Movie> // recupera todos os filmes da tabela do banco de dados
     @Update
-    fun updateMovie(movie: Movie)
+    fun updateMovie(movie: Movie) // atualiza um objeto Movie no banco de dados
     @Delete
-    fun deleteMovie(movie: Movie)
+    fun deleteMovie(movie: Movie) // exclui um objeto Movie do banco de dados
+
 }
+
