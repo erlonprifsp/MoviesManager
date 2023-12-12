e64579cc9dc261fdb99f7c0f9f98dd28282ed12b
erlonprifsp <erlon.rodrigues@aluno.ifsp.edu.br>
2023-12-12 13:36:22 -0300

    update MovieDao

--- app/src/main/java/br/edu/ifsp/scl/sdm/moviesmanager/model/dao/MovieDao.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/moviesmanager/model/dao/MovieDao.kt
@@ -1,4 +1,23 @@
 package br.edu.ifsp.scl.sdm.moviesmanager.model.dao
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import br.edu.ifsp.scl.sdm.moviesmanager.model.entity.Movie
+
+@Dao
 interface MovieDao {
+    companion object {
+        const val MOVIE_TABLE = "movie"
+    }
+    @Insert
+    fun createMovie(movie: Movie)
+    @Query("SELECT * FROM $MOVIE_TABLE")
+    fun retrieveMovies(): List<Movie>
+    @Update
+    fun updateMovie(movie: Movie)
+    @Delete
+    fun deleteMovie(movie: Movie)
 }
